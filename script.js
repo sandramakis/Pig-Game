6dc8e3c21b8206056dbfdee7148a9ac064ee787f
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-06-15 01:07:10 +0100

    RMV: Roll dice Switch Statement, ADD: If statement, ADJ: HTML class names from --1 & --2 to --0 & --1

--- script.js
+++ script.js
@@ -1,73 +1,55 @@
 "use strict";
 
 // Storing all needed doms in a variable
+let personEl0 = document.querySelector(".person--0");
+let personEl1 = document.querySelector(".person--1");
 
-let scoreEl1 = document.querySelector(".total-score--1");
-let scoreEl2 = document.querySelector(".total-score--2");
+let totalScoreEl0 = document.querySelector(".total-score--0");
+let totalScoreEl1 = document.querySelector(".total-score--1");
 
-let rolledScoreEl1 = document.querySelector(".saved-score--1");
-let rolledScoreEl2 = document.querySelector(".saved-score--2");
+// let currentScoreEl0 = document.querySelector(".current-score--0");
+// let currentScoreEl1 = document.querySelector(".current-score--1");
 
 let rollDice = document.querySelector("#roll");
 let hold = document.querySelector("#hold");
 
-let sectionEl1 = document.querySelector("section");
-let sectionEl2 = document.querySelector("section");
-
 let dicePicture = document.querySelector(".dice-picture");
 
-scoreEl1.textContent = 0;
-scoreEl2.textContent = 0;
+totalScoreEl0.textContent = 0;
+totalScoreEl1.textContent = 0;
 
-console.log(scoreEl1, scoreEl2, rollDice, hold);
+let activePlayer = 0;
 
-// rolledScoreEl1.textContent = 0;
-// rolledScoreEl2.textContent = 0;
+console.log(totalScoreEl0, totalScoreEl1, rollDice, hold);
 
-let sum = 0;
-// Generate a random number from 1-6
-const dicePictureFunc = function () {
-  let randomNo = Math.trunc(Math.random() * 6) + 1;
+let currentScore = 0;
 
-  // Use the switch statement to determine the dice pics to be shown when it is rolled
-  switch (randomNo) {
-    case 1:
-      dicePicture.innerHTML = `<img src="pics/dice-1.svg.hi.png" />`;
-      break;
-    case 2:
-      dicePicture.innerHTML = `<img src="pics/dice 2.png" />`;
-      break;
-    case 3:
-      dicePicture.innerHTML = `<img src="pics/dice 3.jpg" />`;
-      break;
-    case 4:
-      dicePicture.innerHTML = `<img src="pics/dice 4.png" />`;
-      break;
-    case 5:
-      dicePicture.innerHTML = `<img src="pics/dice 5.png" />`;
-      break;
-    case 6:
-      dicePicture.innerHTML = `<img src="pics/dice 6.jpg" />`;
-      break;
+// When the dice is rolled, Generate a random number from 1-6
+rollDice.addEventListener("click", function () {
+  let dice = Math.trunc(Math.random() * 6) + 1;
 
-    default:
-      "No Dice Rolled";
-      break;
+  // Use the if statement to determine the dice pics to be shown when it is rolled
+  if (dice) {
+    dicePicture.innerHTML = `<img src="pics/dice ${dice}.png" />`;
   }
 
-  sum += randomNo;
-  rolledScoreEl2.textContent = sum;
+  if (dice !== 1) {
+    // If the rolled dice is not 1, then add to the current score
+    currentScore += dice;
+    document.querySelector(`.current-score--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // If the rolled dice is 1, then the current player's score clears and returns to 0
+    document.querySelector(`.current-score--${activePlayer}`).textContent = 0;
 
-  console.log(randomNo);
+    // Then switch from the current player to the next (If the active current player is Player 1(0), switch to Player 2(0))
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
 
-  return randomNo;
-};
+    // Toggle between the active states
+    personEl0.classList.toggle("active");
+    personEl1.classList.toggle("active");
+  }
 
-rollDice.addEventListener("click", function () {
-  dicePictureFunc();
-  // if (sectionEl1.classList.contains("active")) {
-  //   dicePictureFunc();
-  // } else if (sectionEl2.classList.contains("active")) {
-  //   dicePictureFunc();
-  // }
+  return dice;
 });
