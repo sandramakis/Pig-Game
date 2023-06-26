f398cfe2170b95b6ba670665d735db359049459c
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-06-26 17:37:48 +0100

    ADD: New Game start from first Player; Code Restructure

--- script.js
+++ script.js
@@ -16,82 +16,88 @@ const newGameBtn = document.querySelector("#new-game");
 
 let dicePicture = document.querySelector(".dice-picture");
 
-let activePlayer = 0;
-let currentScore = 0;
-let scores = [0, 0]; //creating an array that'll hold the index of the total score for the two players
+let activePlayer, currentScore, playing, scores;
 
 // Initialize game when it restarts
-function init() {
+const init = function () {
+  playing = true;
+  activePlayer = 0;
+  currentScore = 0;
+  scores = [0, 0]; //creating an array that'll hold the index of the total score for the two players
+
   currentScoreEl0.textContent = 0;
   currentScoreEl1.textContent = 0;
-
   totalScoreEl0.textContent = 0;
   totalScoreEl1.textContent = 0;
 
-  activePlayer = 0;
-  currentScore = 0;
-  scores = [0, 0];
+  personEl0.classList.add("active");
+  personEl1.classList.remove("active");
 
   dicePicture.classList.toggle("hidden");
-}
+};
+init();
 
-// When the dice is rolled, Generate a random number from 1-6
-rollDiceBtn.addEventListener("click", function () {
-  dicePicture.classList.remove("hidden");
-  let dice = Math.trunc(Math.random() * 6) + 1;
-
-  // Use the if statement to determine the dice pics to be shown when it is rolled
-  if (dice) {
-    dicePicture.innerHTML = `<img src="pics/dice ${dice}.png" />`;
-  }
-
-  if (dice !== 1) {
-    // If the rolled dice is not 1, then add to the current score
-    currentScore += dice;
-    document.querySelector(`.current-score--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    /* ** If the rolled dice is 1, then the current player's score clears and returns to 0
-     * Then switches to the next player */
-
-    switchPlayer();
-  }
-
-  return dice;
-});
-
+// switchPlayer Function
 const switchPlayer = () => {
-  document.querySelector(`.current-score--${activePlayer}`).textContent = 0;
-  // Switch from the current player to the next (If the active current player is Player 1(0), switch to Player 2(1))
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  currentScore = 0;
-
   // Toggle between the active states
   personEl0.classList.toggle("active");
   personEl1.classList.toggle("active");
+
+  document.querySelector(`.current-score--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+
+  // Switch from the current player to the next (If the active current player is Player 1(0), switch to Player 2(1))
+  activePlayer = activePlayer === 0 ? 1 : 0;
 };
 
-// Hold Score Btn Function
+// When the dice is rolled, Generate a random number from 1-6
+rollDiceBtn.addEventListener("click", function () {
+  if (playing) {
+    dicePicture.classList.remove("hidden");
+    let dice = Math.trunc(Math.random() * 6) + 1;
 
-holdBtn.addEventListener("click", function () {
-  // When the hold button is clicked, the score[0] or score[1] is added to the current score
-  scores[activePlayer] += currentScore;
+    // Use the if statement to determine the dice pics to be shown when it is rolled
+    if (dice) {
+      dicePicture.innerHTML = `<img src="pics/dice ${dice}.png" />`;
+    }
 
-  // Score is displayed in the activePlayer
-  if (scores[activePlayer] < 100) {
-    // if score is < 100, the score should keep updating
+    if (dice !== 1) {
+      // If the rolled dice is not 1, then add to the current score
+      currentScore += dice;
+      document.querySelector(`.current-score--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to the next player */
 
-    document.querySelector(`.total-score--${activePlayer}`).textContent =
-      scores[activePlayer];
-  } else {
-    document.querySelector(`.total-score--${activePlayer}`).textContent =
-      scores[activePlayer];
-    alert(`🏆 Player ${activePlayer + 1} wins!`);
-    init();
+      switchPlayer();
+    }
+    return dice;
   }
-
-  switchPlayer();
 });
 
-// New Game Function
+// Hold Score Btn Function
+holdBtn.addEventListener("click", function () {
+  // When the hold button is clicked, the score[0] or score[1] is added to the current score
+  if (playing) {
+    scores[activePlayer] += currentScore;
+
+    // Score is displayed in the activePlayer
+    if (scores[activePlayer] >= 20) {
+      document.querySelector(`.total-score--${activePlayer}`).textContent =
+        scores[activePlayer];
+
+      alert(`🏆 Player ${activePlayer + 1} wins!`);
+      playing = false;
+      init();
+    } else {
+      // if score is < 100, the score should keep updating
+
+      document.querySelector(`.total-score--${activePlayer}`).textContent =
+        scores[activePlayer];
+      switchPlayer();
+    }
+  }
+});
+
+// New Game Btn Function
 newGameBtn.addEventListener("click", init);
